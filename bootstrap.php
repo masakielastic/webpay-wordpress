2f8724a6a2f4e8c5ecc167fee266eb516df7fe49
Masaki Kagaya <masakielastic@gmail.com>
2014-07-22 12:34:00 +0900

    added infomation for plugin

---
+++ bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WebPay Simple Checkout
+Plugin URI: https://github.com/masakielastic/webpay-wordpress
+Description: Add WebPay Checkout forms using shortcodes.
+Version: 0.1
+Author: Masaki Kagaya
+Author URI:  https://github.com/masakielastic
+License: GPLv2 or later
+Text Domain: webpay-checkout
+Domain Path: /languages
+*/
