ce5136f55101c876af6484691131a215009cb25c
Masaki Kagaya <masakielastic@gmail.com>
2014-07-24 11:44:06 +0900

    fixed function's name

--- webpay-checkout-settings.php
+++ webpay-checkout-settings.php
@@ -2,11 +2,11 @@
 
 add_action( 'admin_menu', function() {
 	$slug = 'webpay-checkout';
-	add_options_page( __( 'Settings Page for WebPay Checkout', $slug ),
+	add_options_page( __( 'Settings Page for Simple WebPay Checkout', $slug ),
 		'WebPay Checkout', 'manage_options', $slug, 'webpay_checkout_options_page' );
 } );
 
-function webpay_options_page() {
+function webpay_checkout_options_page() {
 	$slug = 'webpay-checkout';
-	echo '<h2>'.__('Settings Page for WebPay Checkout', $slug).'</h2>'
+	echo '<h2>'.__( 'Settings Page for Simple WebPay Checkout', $slug ).'</h2>';
 }
